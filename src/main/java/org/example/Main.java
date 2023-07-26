c39efd93744e5052fad5b3be2fb1b35662782efb
KandratUA <kondratinskyi@gmail.com>
2023-07-26 19:51:47 +0300

    1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+class Main {
+
+    public static void main(String[] args) {
+        String name = "Dmytro";
+        String lastName = "Kondratynsky";
+
+        Person person = new Person(name, lastName);
+        Gson gson = new Gson();
+        String json = gson.toJson(person);
+
+        System.out.println(json);
+    }
+}
+
+class Person {
+    String name;
+    String lastName;
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
